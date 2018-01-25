788fd99a46a9fc0106b20f68e0cc157849829007
dopatraman <prakash@asanet.com>
2018-01-24 19:10:38 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "abode-scripts",
+    version = "0.0.1",
+    author = "Prakash Venkatraman",
+    author_email = "prakash@asanet.com",
+    description = ("Convenience scripts for Abode"),
+    license = "BSD",
+    url = "https://github.com/dopatraman/abode-scripts",
+    packages=['openpyxl'],
+    long_description=read('README.md')
+)
